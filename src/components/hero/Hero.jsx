bfbfabd5887f338b4710f9ad7f88568470c0c1d1
akarsu35 <cumhurakarsu@gmail.com>
2024-07-19 23:55:59 +0300

    hero updated

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,27 +1,13 @@
+
 export default function Hero() {
   return (
     <div className="">
       <div className="relative">
-        <video
-          controls
-          loop
-          muted // Videoyu otomatik olarak sessize al
-          autoPlay // Videoyu otomatik olarak başlat
-          playsInline // Mobil cihazlarda tam ekran olmadan oynat
-          src="/videos/3.mp4"
-          type="video/mp4"
-          className="w-full"
-        >
-          <source src="/videos/3.mp4" type="video/mp4" />{' '}
-          {/* Farklı formatlar için kaynak ekleme */}
-          Tarayıcınız video etiketini desteklemiyor.
-        </video>
-
-        {/* <img
+        <img
           src="./images/herobg.jpg"
           alt="hero-background"
           className="w-full h-[90vh]"
-        /> */}
+        />
         <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
       </div>
       <div className="absolute top-[40%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-center">
@@ -35,6 +21,7 @@ export default function Hero() {
           <br />
           <p>Hızlı Montaj</p>
           <br />
+          
           <p>Yüksek Kalite</p>
           <br />
           <p>Güvenilir Hizmet</p>
