ff45294516b9d669b17a66e634fbd5814301086b
akarsu35 <cumhurakarsu@gmail.com>
2024-07-27 12:10:25 +0300

    1366x768-7

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,7 +5,7 @@ import { faRankingStar } from '@fortawesome/free-solid-svg-icons'
 import Social from '../social/Social'
 export default function Hero() {
   return (
-    <div className="bg-hero-bg bg-cover min-h-[90vh] 1366x768:min-h-[1250px]">
+    <div className="bg-hero-bg bg-cover  min-h-[90vh] 1366x768:h-[830px]">
       {/* <div className="relative">
         <img
           src="./images/herobg.jpg"
@@ -42,7 +42,7 @@ export default function Hero() {
           <p className="text-2xl">Güvenilir Hizmet</p>
         </div>
       </div>
-      <div className="relative sm:-bottom-20 -bottom-2 ml-10 sm:ml-28">
+      <div className="relative sm:-bottom-20 -bottom-2  ml-10 sm:ml-28">
         <Social />
       </div>
     </div>
