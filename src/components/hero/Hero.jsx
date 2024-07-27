3425b6b09412048acbd3a552789a71522264a591
akarsu35 <cumhurakarsu@gmail.com>
2024-07-27 12:25:55 +0300

    screen media1

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -42,7 +42,7 @@ export default function Hero() {
           <p className="text-2xl">Güvenilir Hizmet</p>
         </div>
       </div>
-      <div className="relative sm:-bottom-20 -bottom-2  ml-10 sm:ml-28 1366x768:bottom-0">
+      <div className="relative sm:-bottom-20 -bottom-2  ml-10 sm:ml-28 1366x768:bottom-10">
         <Social />
       </div>
     </div>
